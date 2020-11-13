19d335408dd4eda1725ceac3c2fff374cc728720
Md Alamin Hossain <hossainalamin980@gmail.com>
2020-11-13 12:52:21 +0600

    Create api-search.php
    first commit

---
+++ api-search.php
@@ -0,0 +1,18 @@
+<?php
+header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Method: get,put,post,delete');
+include("config.php");
+// $data = json_decode(file_get_contents("php://input"),true);
+// $Student_name = $data['search'];
+$Student_name = ($_SERVER['REQUEST_METHOD'] == 'GET') && $_GET['search'] ? $_GET['search']:die("no such data");
+$sql = "select * from user where name like %'$Student_name'%";
+$result = mysqli_query($conn,$sql) or die("No data found".mysqli_error($conn));
+if(mysqli_num_rows($result)>0){
+    $output = mysqli_fetch_all($result,MYSQLI_ASSOC);
+    echo json_encode($output);
+}else{
+    echo json_encode(array('message'=>'No record found','status'=>false));
+}
+
+?>
